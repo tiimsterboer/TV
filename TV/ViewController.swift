998de0a9afaa53c8971f9bafff3ca2d3c8de1e42
Tim Beemsterboer <tim.beemsterboer@gmail.com>
2018-02-09 11:23:17 -0800

    first commit

--- TV/ViewController.swift
+++ TV/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
+struct TVShow: Decodable {
+    let id : Int32
+    let name : String
+    let type : String
+    let language : String
+    let genres : Array<String>
+}
+
 class ViewController: UIViewController {
+    
+    var shows = [TVShow]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +30,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onRequestTapped(_ sender: Any) {
+        for index in 0...138 {
+        let jsonurl = "https://api.tvmaze.com/shows?page=\(index)"
+        let url = URL(string: jsonurl)
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            do {
+                self.shows = try JSONDecoder().decode([TVShow].self, from: data!)
+                //for show in self.shows {
+                    //print(show.name)
+                    
+                //}
+                print(self.shows)
+            } catch {
+                print("error")
+            }
+        }.resume()
+    }
+    }
 }
 
